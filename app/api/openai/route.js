ef4149ee55cae4987b71ff3691db6969feb3171e
Georgii Momdzhi <momdzhiphoto@gmail.com>
2025-10-28 15:35:00 +0300

    initial

---
+++ app/api/openai/route.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import OpenAi from 'openai'
+
+const openai = new OpenAi ({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+export async function POST (req) {
+    try {
+        const { text } = await req.json()
+
+        const response = await openai.responses.create ({
+            model: 'gpt-4.1-2025-04-14',
+            input: `Придумай анекдот вот с этими словами: ${text}`
+        })
+        console.log (response)
+        const message = response.output_text
+
+        return NextResponse.json ({ reply: message }) || `Нет ответа`
+    } catch (err) {
+        console.error (`Ошибка в запросе: ${err}`)
+        return NextResponse.json ({ err: "Ошибка в запросе"}, { status: 500})
+    }
+}
